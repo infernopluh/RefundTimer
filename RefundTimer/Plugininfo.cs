68e0f9e1ff2647d752b0c8cf442ff3b09a37e12c
walkslow <santinobonomolo142@gmail.com>
2025-05-12 10:26:26 -0400

    Create Plugininfo.cs

---
+++ RefundTimer/Plugininfo.cs
@@ -0,0 +1,13 @@
+
+namespace RefundTimer
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.64will64.gorillatag.buttoncursor";
+        public const string Name = "ButtonCursor";
+        public const string Version = "1.0.0";
+    }
+}
